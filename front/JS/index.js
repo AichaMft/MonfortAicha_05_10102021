975c60adbc262e9380111f43430425c0d66f78e3
Aïcha <mft.aicha@gmail.com>
2021-10-25 16:41:01 +0200

    page produit

--- front/JS/index.js
+++ front/JS/index.js
@@ -6,7 +6,7 @@ fetch(input = "http://localhost:3000/api/products")
     for (let i = 0; i < data.length; i++) {
         let items = document.querySelector('#items');
         htmlInput += 
-                `<a href="./product.html?id= ${data[i]._id}"> 
+                `<a href="./product.html?id=${data[i]._id}"> 
                 <article> 
                 <img src="${data[i].imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1"> 
                 <h3 class="productName">${data[i].name}</h3> 
@@ -15,6 +15,3 @@ fetch(input = "http://localhost:3000/api/products")
                 </a>`;
         items.innerHTML = htmlInput ;
 }});
-
-
-}
--- front/JS/product.js
+++ front/JS/product.js
@@ -1,27 +1,47 @@
 //Appeler l'id de la page
-var location = "C:/Users/Msi/OneDrive/Documents/Kanap/P5-kanap/front/html/product.html?id=";
-var str = location.href ;
-var url = new URL(str);
-var productId = url.searchParams.get("id");
+let currentUrl = window.location.search;
+let url = new URLSearchParams(currentUrl);
+let productId = url.get("id");
 console.log(productId);
-//Récuperer l'id depuis l'api'
+let itemImage = document.querySelector("article .item__img");
+console.log(itemImage);
+const itemName = document.getElementById("title");
+const itemPrice = document.getElementById("price");
+const itemDescription = document.getElementById("description");
+const itemColors = document.getElementById("colors");
+const itemQuantity = document.getElementById("quantity");
+const confirmItem = document.getElementById("addToCart");
+confirmItem.setAttribute("type", "submit");
+console.log(confirmItem);
+
+//Récuperer l'id depuis l'api
 fetch(`http://localhost:3000/api/products/${productId}`)
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
-        allItems(data);
-        function allItems(data) {
-        const itemImage = document.getElementsByClassName("item__img");
-        itemImage.innerHTML += 
-        `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-        const itemName = document.getElementById("title");
-        itemName.innerHTML += 
-        ` <h1 id="title">${data.name}</h1>`;
-        const itemPrice = document.getElementById("price");
-        itemPrice.innerHTML += 
-        `<span id="price">${data.price}</span>€</p>`;
-        const itemDescription = document.getElementById("description");
-        itemDescription.innerHTML += 
-        `<p id="description">${data.description}</p>`;
-    }
-});
+        itemImage.innerHTML +=
+            `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+        itemName.innerText += data.name;
+        itemPrice.innerText += data.price;
+        itemDescription.innerText += data.description;
+        const color = data.colors;
+        console.log(color[1]);
+        for (let i = 0; i < color.length; i++) {
+            itemColors.innerHTML +=
+                `<option value="${data.colors}">${data.colors[i]}</option>`
+        };
+    });
+        confirmItem.addEventListener("click", function(event){
+            function Event(event){
+            if(itemColors.value.length >= 1){
+                alert("Veuillez sélectionner la couleur svp")
+            }
+            if(itemQuantity.valueAsNumber == 0, itemQuantity.valueAsNumber < 100){
+                alert ("Le champ est incorrect")
+            }
+            else {
+                console.log("direction le panier")
+            }
+            Event();
+        }
+    });
